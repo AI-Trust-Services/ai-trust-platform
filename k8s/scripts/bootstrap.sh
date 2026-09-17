8d5dafb9ed0ca6fb13ec3cd5cec0f992f0be887c
Sebastian Rochacki <sebastian.rochacki@sap.com>
2026-09-17 16:40:15 +0200

    Replace PVC by ConfigMap (issue on multi node system)

--- k8s/scripts/bootstrap.sh
+++ k8s/scripts/bootstrap.sh
@@ -110,6 +110,9 @@ rules:
   - apiGroups: ["batch"]
     resources: ["jobs"]
     verbs: ["get", "list", "watch"]
+  - apiGroups: [""]
+    resources: ["configmaps"]
+    verbs: ["get", "create", "patch", "update"]
 ---
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
